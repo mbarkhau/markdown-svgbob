51a27557544ebf9d8eaffcda37bbf6d44a58caf2
Manuel Barkhau <mbarkhau@gmail.com>
2019-05-18 23:47:29 +0200

    bump version to v201905.0006-beta

--- src/markdown_svgbob/__init__.py
+++ src/markdown_svgbob/__init__.py
@@ -10,7 +10,7 @@ uses svgbob to generate images from ascii
 diagrams in fenced code blocks.
 """
 
-__version__ = "v201905.0005-beta"
+__version__ = "v201905.0006-beta"
 
 
 from markdown_svgbob.extension import SvgbobExtension
