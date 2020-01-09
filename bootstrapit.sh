a64dd81eaffd144b0f541386261b6854a390599a
Manuel Barkhau <mbarkhau@gmail.com>
2020-01-09 17:53:33 +0000

    bump version to v202001.0010-beta

--- bootstrapit.sh
+++ bootstrapit.sh
@@ -34,7 +34,7 @@ MODULE_NAME="markdown_svgbob"
 GIT_REPO_NAMESPACE="mbarkhau"
 GIT_REPO_DOMAIN="gitlab.com"
 
-PACKAGE_VERSION="v202001.0009-beta"
+PACKAGE_VERSION="v202001.0010-beta"
 
 # These must be valid (space separated) conda package names.
 # A separate conda environment will be created for each of these.
