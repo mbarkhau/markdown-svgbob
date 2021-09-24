aa2558db1e1ed775935f027b056288689cabe0e4
Manuel Barkhau <mbarkhau@gmail.com>
2021-09-24 22:22:11 +0000

    lint fix

--- test/test_svgbob.py
+++ test/test_svgbob.py
@@ -15,8 +15,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import re
 import io
+import re
 import textwrap
 
 import markdown as md
